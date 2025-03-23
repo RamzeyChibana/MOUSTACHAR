13580c2eefb19a0b1e22cee2e3a9d1796f231001
ramzeychibana <ramzichibana31@gmail.com>
2025-03-23 14:20:17 +0100

    workstation updates

--- Rag.py
+++ Rag.py
@@ -5,94 +5,29 @@ import ollama
 from chromadb import Documents, EmbeddingFunction, Client
 from typing import List
 import json
+import re
 
-# 1. Prepare Documents
-# 1. Prepare Documents
-with open(f"laws_json\\All_laws.json","r",encoding="utf-8") as file :
-    laws = json.load(file)
-documents = [str(l[list(l.keys())[0]]) for l in laws]
-print(documents[0:5])
-# Save documents to a text file (for demonstration)
-with open("data.txt", "w",encoding="utf-8") as f:
-    f.write("\n".join(documents))
-
-
-# Save documents to a text file (for demonstration)
-with open("data.txt", "w",encoding="utf-8") as f:
-    f.write("\n".join(documents))
-
-# 2. Document Processing
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def chunk_documents():
-    with open("data.txt",encoding="utf-8") as f:
-        text = f.read()
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=50,
-        length_function=len
-    )
-    return text_splitter.split_text(text)
-
-chunks = chunk_documents()
-
-# 3. Embedding Function using Ollama
-class OllamaEmbeddingFunction(EmbeddingFunction):
-    def __call__(self, texts: Documents):
-        embeddings = []
-        for text in texts:
-            response = ollama.embeddings(
-                model="nomic-embed-text",
-                prompt=text
-            )
-            embeddings.append(response["embedding"])
-        return embeddings
-
-
-client = Client()
-collection = client.create_collection(
-    name="rag_demo",
-    embedding_function=OllamaEmbeddingFunction()
-)
-
-# documents to collection
-collection.add(
-    ids=[str(i) for i in range(len(chunks))],
-    documents=chunks
-)
-
-
-def rag_query(query: str, temperature: float = 0.7):
-    #  embedding
-    query_embedding = ollama.embeddings(
-        model="nomic-embed-text",
-        prompt=query
-    )["embedding"]
-    
-
-    
-    results = collection.query(
-        query_embeddings=[query_embedding],
-        n_results=3
-    )
-
-    context = "\n\n".join(results["documents"][0])
-    
-    
+def extract_fields(content,fields):
+    """Extract legal keywords from content"""
     response = ollama.generate(
-        model="mistral:latest",
-        prompt=f"""Answer the question using only this context:
-        {context}
-        
-        Question: {query}
-        Answer:"""
+        model="deepseek-r1:32b",
+        prompt=f"Extract filed for :\n{content}\n from those fields:{fields} fields: output as JSON"
     )
-    
-    return response["response"]
+    raw_text = response["response"].strip()
+    raw_output = response["response"].strip()
+    if '```json' in raw_output:
+        raw_output = raw_output.split('```json')[1].split('```')[0]
+   
+        
+    print(raw_output)
+    result = json.loads(raw_output)
+    return result["field"]
 
+with open("new_laws/chapters.txt", "r") as file:
+    chapters = file.read()
 
-if __name__ == "__main__":
-    query = "Who is nadir?"
-    print("Question:", query)
-    print("Answer:", rag_query(query))
+with open("new_laws/fields.txt", "r") as file:
+    fields = file.read()
+
+query = "le prix du timbre du passeport"
+print(extract_fields("Les règles posées par les articles 4 à 46 qui suivent sont d’application générale. Toutefois, desdispositions spéciales à chacun des produits peuvent les compléter ou y déroger.2",fields))
--- test.py
+++ test.py
@@ -3,18 +3,18 @@ import os
 import numpy as np
 import pandas as pd
 
-files = os.listdir("laws_json")
+files = os.listdir("new_laws")
 
 laws = []
 for file in files:
  
-    with open(f"laws_json\\{file}","r",encoding="utf-8") as file :
+    with open(f"new_laws\\{file}","r",encoding="utf-8") as file :
         law = json.load(file)
         laws+=law
 
 print(f"there is {len(laws)}")
 
-with open("laws_json/All_laws.json", "w", encoding="utf-8") as f:
+with open("new_laws/All_laws.json", "w", encoding="utf-8") as f:
     json.dump(laws, f, ensure_ascii=False, indent=4)
 
 
